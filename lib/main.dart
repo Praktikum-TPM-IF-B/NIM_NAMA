b9962e7da152b5440e66965b504f82b4935f9a94
Maldimz <pratamadimas03@gmail.com>
2024-03-05 09:49:07 +0700

    feat: login page pertemuan 3

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo', // judul aplikasi
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Loginpage(), // panggil class Homepage
     );
   }
--- lib/pages/Loginpage.dart
+++ lib/pages/Loginpage.dart
@@ -8,6 +8,10 @@ class Loginpage extends StatefulWidget {
 }
 
 class _LoginpageState extends State<Loginpage> {
+  String username = "";
+  String password = "";
+  bool isError = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,33 +24,103 @@ class _LoginpageState extends State<Loginpage> {
               'Login',
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
+            _usernameFields(),
+            _passwordFields(),
             Container(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
+              padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: Text(
+                isError ? "Username atau password salah" : "",
+                style: TextStyle(color: Colors.red, fontSize: 12),
               ),
             ),
-            Container(
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  print("login");
-                },
-                child: Text('Login'))
+            _buttonLogin(context),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _usernameFields() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: TextField(
+        onChanged: (value) {
+          setState(() {
+            username = value;
+          });
+
+          print("username: " + username);
+        },
+        decoration: InputDecoration(
+            // menggunakan errorText
+            errorText: isError ? "username salah" : null,
+            labelText: 'Username',
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+      ),
+    );
+  }
+
+  Widget _passwordFields() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        obscureText: true,
+        onChanged: (value) {
+          setState(() {
+            password = value;
+          });
+
+          print("password: " + password);
+        },
+
+        // cara menggunakan enabled Border
+        decoration: InputDecoration(
+            labelText: 'Password',
+            // errorBorder: , styling ketika error
+            // focusedBorder: , styling ketika fokus pada fields
+            // disabledBorder: , styling untuk border ketika fields disable
+            enabledBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: isError ? Colors.red : Colors.blue),
+                borderRadius: BorderRadius.circular(10)),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+      ),
+    );
+  }
+
+  Widget _buttonLogin(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+          onPressed: () {
+            // print("login");
+            print("username: " + username);
+            print("password: " + password);
+
+            String text = "Login Sukses";
+
+            if (username == "flutter123" && password == "password") {
+              setState(() {
+                isError = false;
+              });
+            } else {
+              setState(() {
+                isError = true;
+              });
+              text = "Login Gagal";
+            }
+
+            SnackBar snackBar = SnackBar(
+              behavior: SnackBarBehavior.floating,
+              content: Text(text),
+            );
+
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          child: Text('Login')),
     );
   }
 }
