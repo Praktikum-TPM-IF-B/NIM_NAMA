f1dafe4afa34c3df7a3a7a19c912f72d5a423538
Maldimz <pratamadimas03@gmail.com>
2024-03-05 08:06:59 +0700

    feat: clean page with login page

--- lib/pages/Loginpage.dart
+++ lib/pages/Loginpage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Loginpage extends StatefulWidget {
   const Loginpage({Key? key}) : super(key: key);
